6393bfbf4c5f483337472d6d7fff0207b50d2de4
gkalyankumar20 <97790187+gkalyankumar20@users.noreply.github.com>
2022-01-16 14:27:39 +0530

    toastermessagecompleted

--- B17AEcom/src/app/app.component.ts
+++ B17AEcom/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'B17AEcom';
+  title = 'materialpractice';
+
+  constructor(private data : DataService) { }
+
+ actionwishList(){
+    this.data.itemAddedWish();
+  }
+
+  actioncartitem(){     
+    this.data.itemAddedCart();
+  }
+
+  actionremoved(){
+    this.data.itemRemovedWish();
+  }
+
+  actionclearWish(){
+    this.data.wishClear();
+  }
+
+  actionincreasecartItem(){
+    this.data.itemIncCart();
+  }
+
+  actiondecCartItem(){
+    this.data.itemDecCart();
+  }
+
+  actioncartItemdelete(){
+    this.data.productDeleted();
+  }
+
+  actionclearCart(){
+    this.data.cartClear();
+  }
+
+  actionplaceOrder(){
+    this.data.orderPlace();
+  }
 }
--- B17AEcom/src/app/app.module.ts
+++ B17AEcom/src/app/app.module.ts
@@ -4,13 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from "@angular/material/icon";
+import { MatSliderModule } from "@angular/material/slider";
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSliderModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
